cfb77d6c79a5d40594fc778a7168a3f2cc19d6a9
abdurrahmaan <ahunter786@mail.com>
2016-03-16 13:03:01 +0200

    correct variable declaration ...code works

--- tut1FBW/tut1FBW/fbw.cpp
+++ tut1FBW/tut1FBW/fbw.cpp
@@ -4,8 +4,8 @@ int main()
 
 {
 	int var1, var2, var3; // declaration of variables for the conditions
-	int i // variable to use in the for loop 
-		for (i = 1; i < 21; i++) // for loop running from 1 to 20
+	 // variable to use in the for loop 
+		for (int i=1; i < 21; i++) // for loop running from 1 to 20
 		{
 
 			var1 = i % 5;
