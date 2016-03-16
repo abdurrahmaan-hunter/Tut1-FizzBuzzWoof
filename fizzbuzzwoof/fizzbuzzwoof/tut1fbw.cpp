e343a8353336424e5523982b6457a51122dc0e07
abdurrahmaan <ahunter786@mail.com>
2016-03-16 12:52:13 +0200

    initial code

--- fizzbuzzwoof/fizzbuzzwoof/tut1fbw.cpp
+++ fizzbuzzwoof/fizzbuzzwoof/tut1fbw.cpp
@@ -3,7 +3,13 @@ using namespace std ;
 int main()
 
 {
-	int var1 var2 var3; // declaration of variables for the conditions
+	int var1 ,var2 ,var3; // declaration of variables for the conditions
+	int i // variable to use in the for loop 
+		for (i = 1; i < 21; i++) // for loop running from 1 to 20
+		{
 
+
+
+		}
 }
 
---
+++ tut1FBW/tut1FBW/fbw.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+	int var1, var2, var3; // declaration of variables for the conditions
+	int i // variable to use in the for loop 
+	
+}
+
