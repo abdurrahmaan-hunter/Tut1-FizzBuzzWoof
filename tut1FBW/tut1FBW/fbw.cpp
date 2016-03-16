3edb7ccbbef039f210508dba916cae613167be01
abdurrahmaan <ahunter786@mail.com>
2016-03-16 13:00:13 +0200

    completion of for loop

--- tut1FBW/tut1FBW/fbw.cpp
+++ tut1FBW/tut1FBW/fbw.cpp
@@ -8,8 +8,18 @@ int main()
 		for (i = 1; i < 21; i++) // for loop running from 1 to 20
 		{
 
+			var1 = i % 5;
+			var2 = i % 3;
+			var3 = var1 + var2;
 
-
+			if (var3 == 0)
+				cout << "fizzbuzz" << endl;
+			else if (var1 == 0)
+				cout << "buzz" << endl;
+			else if (var2 == 0)
+				cout << "fizz" << endl;
+			else
+				cout << i << endl;
 		}
 }
 
