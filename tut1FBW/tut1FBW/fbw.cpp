9f34c4b53a7ae3355c52182d4bdca72052311bbc
abdurrahmaan <ahunter786@mail.com>
2016-03-16 12:54:04 +0200

    for loop

--- tut1FBW/tut1FBW/fbw.cpp
+++ tut1FBW/tut1FBW/fbw.cpp
@@ -5,6 +5,11 @@ int main()
 {
 	int var1, var2, var3; // declaration of variables for the conditions
 	int i // variable to use in the for loop 
-	
+		for (i = 1; i < 21; i++) // for loop running from 1 to 20
+		{
+
+
+
+		}
 }
 
